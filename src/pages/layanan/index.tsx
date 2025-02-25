916022fbd5d40a54fe4c49e0ec46636596e93f80
ricky <rickyr1307@gmail.com>
2025-02-25 09:14:32 +0700

    style:css layanan page

--- src/pages/layanan/index.tsx
+++ src/pages/layanan/index.tsx
@@ -30,19 +30,19 @@ const Layanan = () => {
   return (
     <section className="w-full min-h-screen bg-[url('/src/assets/client/images/sm-bg.svg')] lg:bg-[url('/src/assets/client/images/lg-bg.svg')] z-10 bg-cover  bg-center bg-no-repeat">
       <Header />
-      <div className="md:pt-24 pt-24 w-[90%] mx-auto grid h-screen grid-rows-10">
+      <div className="w-[90%] mx-auto grid h-screen grid-rows-10 gap-3">
         {/* <div className="flex-center flex-col">
           <h2 className="font-medium text-xl md:text-2xl">{data?.name}</h2>
           <p className="sm:text-base text-sm text-center">
             {data?.alamat} {data?.kota?.name} {data?.kota?.provinsi?.name}
           </p>
         </div> */}
-        <div className="grid">
+        <div className="grid row-start-2">
           <h2 className="font-medium text-xl md:text-2xl text-center">{dataCabang && JSON.parse(dataCabang).nama}</h2>
           <p className="text-xs lg:text-bsae text-center">{dataCabang && JSON.parse(dataCabang).alamat}</p>
           <h2 className="font-medium text-left text-xl  md:text-2xl flex flex-start">Pilih Layanan :</h2>
         </div>
-        <div className="overflow-auto row-span-8 wrapper-cabang rounded-3xl p-3">
+        <div className="overflow-auto row-start-3 row-end-10 wrapper-cabang rounded-3xl p-3">
           {isLoadingLayanan ? (
             <SkeletonCard type="card" count={2} />
           ) : dataLayanan?.data?.length > 0 ? (
